377ee4701a281016317b2ef61a1b3eb9a38f9041
fxbite <letruongthinh1703@gmail.com>
2022-06-27 16:11:04 +0700

    Remove morgan

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const route = require('./routes')
-const morgan = require('morgan')
 const dotenv = require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080
-
-app.use(morgan('dev'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
